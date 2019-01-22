9438f24441434e97b64dcdf601f0b56af7c59ec5
Sergio Calera <sergiocalera@gmail.com>
2019-01-21 23:47:12 -0600

    Se trabajan diferentes funciones con return

--- methods.rb
+++ methods.rb
@@ -2,6 +2,24 @@ def saludar(nombre, saludo)
 	puts "Hola #{nombre}, que tengas #{saludo}"
 end
 
+def suma(num1, num2)
+	suma = num1 + num2
+	#puts "La suma de #{num1} y #{num2} es #{suma}"
+	return suma
+end
+
+def resta(num1, num2)
+	resta = num1 -num2
+	return resta
+end
+
 print "Ingresa tu nombre: "
 nombre = gets.chomp
 saludar( nombre , 'buena tarde')
+
+a = 1
+b = 2
+resultado = suma(a,b)
+puts "La suma de #{a} y #{b} es: #{resultado}"
+
+puts "La resta de #{a} y #{b} es: #{resta(a,b)}"
