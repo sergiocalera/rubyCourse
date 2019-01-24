833241c640ff24f8702deb056856c57b92904315
sergiocalera <sergiocalera@gmail.com>
2019-01-24 08:04:59 +0000

    Se comentan reglas del uso do y {} en ciclos

--- while_loop.rb
+++ while_loop.rb
@@ -1,6 +1,9 @@
 vidas = 3
 
-while vidas > 0
+#El uso del do puede ser opcional dentro de todos los ciclos solo que este
+#debe de ir al final de las condiciones, al igual el uso del {} puede sus-
+#tituir la utilización del "do" y "end"
+while vidas > 0 do
 	puts "Estoy jugando porque tengo vidas"
 	vidas -= 1
 end
